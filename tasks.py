213033b74b8e675eacdf829d531afc2e5d025a34
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-19 13:14:55 +0100

    chore: add task build_image

--- tasks.py
+++ tasks.py
@@ -21,3 +21,17 @@ def safety(ctx):
     ctx.run(gen_requirements)
     pipenv_run_safety = "pipenv run safety check --file=./requirements.txt"
     ctx.run(pipenv_run_safety)
+
+@task
+def build_image(ctx, tags="digdir/fdk-portal-monitoring:latest", staging=False):
+    if staging:
+        ctx.run(pipenv_install)
+    gen_requirements = "pipenv lock -r >requirements.txt"
+    ctx.run(gen_requirements)
+    tag = ""
+    for t in tags.split(","):
+        tag = tag + ' -t ' + t
+
+    print("building image with tag " + tag)
+    build_cmd = "docker build . " + tag
+    ctx.run(build_cmd)
